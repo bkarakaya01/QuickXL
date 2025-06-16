0c1e2ddb7f0918ea9798b53b7b28e2ae666ffd39
bkarakaya01 <bkarakaya01@gmail.com>
2025-06-17 00:21:33 +0300

    refactor: updates default sheet name

--- src/QuickXL.Core/Options/QuickXLOptions.cs
+++ src/QuickXL.Core/Options/QuickXLOptions.cs
@@ -2,6 +2,6 @@
 
 public class QuickXLOptions
 {
-    public string DefaultSheetName { get; set; } = "Sheet1";
+    public string DefaultSheetName { get; set; } = "QuickXL";
     public int DefaultFirstRowIndex { get; set; } = 0;    
 }
