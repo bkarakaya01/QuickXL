c5eca7c0f2719bf56f7c837bb723ff8c4acd28d1
bkarakaya01 <bkarakaya01@gmail.com>
2025-06-22 18:21:53 +0300

    chore: set LangVersion to latest for netstandard2.0

--- src/QuickXL.Core/Result/XLResultError.cs
+++ src/QuickXL.Core/Result/XLResultError.cs
@@ -1,4 +1,13 @@
 ﻿namespace QuickXL.Core.Result;
 
-public sealed record XLResultError(string Code, string Message);
+public sealed record XLResultError
+{
+    public XLResultError(string code, string message)
+    {
+        Code = code;
+        Message = message;
+    }
+    public string Code { get; set; }
+    public string Message { get; set; }
+}
 
